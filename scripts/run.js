16f930621b11c498987a843b1b767e4f44e9090f
Carlos Di Matteo <carlosdima97@gmail.com>
2022-03-27 18:28:40 -0500

    modified test code

--- scripts/run.js
+++ scripts/run.js
@@ -10,14 +10,12 @@ const main = async () => {
   await dropContract.deployed()
   console.log('contract address = ',dropContract.address)
   const [owner] = await ethers.getSigners();
-  // console.log({a:addressToDrop.address,b:addressToDrop2.address})
+  console.log({a:addressToDrop.address,b:addressToDrop2.address})
   console.log('contract owner = ',await dropContract.owner(),owner.address)
   const uri = await dropContract.uri(1)
-  const addressToDrop = {address:"0xFf4240ee0Ccb10C152A725E2B673EBfE8e9A8b00"}
   const dropTxn = await dropContract.deployToken([addressToDrop.address],1000)
   await dropTxn.wait()
   const droppedBalance = await dropContract.balanceOf(addressToDrop.address,1)
-  const addressToDrop2 = {address:"0x87711156017e540456343DD99BbbbdB10BC45480"}
   const dropTxn2 = await dropContract.deployToken([addressToDrop2.address],1000)
   await dropTxn2.wait()
   const droppedBalance2 = await dropContract.balanceOf(addressToDrop2.address,1)
